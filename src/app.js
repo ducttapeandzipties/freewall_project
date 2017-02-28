459eb368ac579980c77fe9994d1fce03caa4f353
Dan Wagner <dan.nmi.wagner@gmail.com>
2017-02-28 12:02:57 -0500

    added comments to code and a small outline

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
+// src/app.js
+
+//creates an angular app named "freewallApp"
+
 angular.module("freewallApp", []);
+
+
+
+
+// TODO
+//
+// break these down into smaller steps
+//
+// read database on page load and display most recent content to tall
+// read index from database and display
+//
+// on form submission update wall, index, and save form data to database
+//
--- src/wall.js
+++ src/wall.js
@@ -1,6 +1,10 @@
-angular.module('freewallApp')
+// src/wall.js
+
+
+
+angular.module('freewallApp')   //creates an angular directive to fill the wall
 .directive('wall', function(){
   return {
-    template: "<br><br><br><br>User content goes here<br><br><br><br><br>"
+    template: "<br><br><br><br><h1>User content goes here</h1><br><br><br><br><br>"
   };
 })
