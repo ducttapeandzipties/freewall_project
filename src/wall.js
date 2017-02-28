cdd572c10427484eca35d418a3fd4895f48e4921
Dan Wagner <dan.nmi.wagner@gmail.com>
2017-02-28 12:08:23 -0500

    fixed merge issue, must remember to pull before working

--- src/wall.js
+++ src/wall.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // src/wall.js
 
 
@@ -7,11 +6,6 @@ angular.module('freewallApp')   //creates an angular directive to fill the wall
 .directive('wall', function(){
   return {
     template: "<br><br><br><br><h1>User content goes here</h1><br><br><br><br><br>"
-=======
-angular.module('freewallApp')
-.directive('wall', function(){
-  return {
-    template: "<br><br><br><br>User content goes here<br><br><br><br><br>"
->>>>>>> add4e9ce3b40651aed209b2249483227c589f50b
+
   };
 })
