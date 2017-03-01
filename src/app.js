d3ab9e8912321ef040f959f5615d6656e6529148
Dan Wagner <dan.nmi.wagner@gmail.com>
2017-02-28 19:41:17 -0500

    planning

--- src/app.js
+++ src/app.js
@@ -9,10 +9,14 @@ angular.module("freewallApp", []);
 
 // TODO
 //
+// code express app into code
+// setup node server
+//
 // break these down into smaller steps
 //
 // read database on page load and display most recent content to tall
 // read index from database and display
 //
 // on form submission update wall, index, and save form data to database
+//
 //
