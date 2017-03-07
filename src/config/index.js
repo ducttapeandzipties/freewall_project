078832d5710c8ede82366815d621a83004f3451b
Dan Wagner <dan.nmi.wagner@gmail.com>
2017-03-06 23:06:01 -0500

    created empty express app

---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+// config/index.js
+
+//express app config
+
+
+
+const config = {
+  appName: 'freewall app',
+  port: 8080,
+}
+
+module.exports = config;
--- src/server.js
+++ src/server.js
@@ -1,1 +1,16 @@
+// src/server.js
+
 // create node server here
+
+
+const express    = require('express');    //includes express
+const config     = require('./config');   //includes express app config
+const router     = require('./routes');   //includes express routes
+
+const app = express();  //creates an express app called "app"
+
+
+// creates a server on port 8080
+app.listen(config.port, function() {
+  console.log("server running on port " + config.port);
+});
