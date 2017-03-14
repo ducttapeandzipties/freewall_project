c9cdc514893f4c06c02b51a8a6f4497e2c96d06c
Dan Wagner <dan.nmi.wagner@gmail.com>
2017-03-14 02:34:36 -0400

    set up static server and linked css file. woot

--- src/server.js
+++ src/server.js
@@ -2,16 +2,20 @@
 
 
 
-const express    = require('express');    //includes express
-const config     = require('./config');   //includes express app config
-const router     = require('./routes');   //includes express routes
-
+const express = require('express');    //includes express
+const config = require('./config');   //includes express app config
+const router = require('./routes');   //includes express routes
 const mongoose = require('mongoose');  //includes mongoose
+const path = require('path');
 
 const app = express();  //creates an express app called "app"
+
+// app.use(express.static(__dirname + '/src'));   //serve static files
+app.use(express.static(path.join(__dirname, '../src/public')));
+
 app.use(router);
 app.set('view engine', 'pug');
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/views');
 
 // creates a server on port 8080
 app.listen(config.port, function() {
