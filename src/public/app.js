9c2d2a24661d6404f7cc098ad3cc917d7dad6559
Dan Wagner <dan.nmi.wagner@gmail.com>
2017-03-21 19:04:53 -0400

    rearrangine scaffolding

--- src/public/app.js
+++ src/public/app.js
@@ -7,9 +7,6 @@ angular.module("freewallApp", []); //creates an angular app named "freewallApp"
 
 // TODO
 //
-// create main view and route to get to view
-// move code in this file to main view
-//
 //
 // read database on page load and display most recent content to wall
 // read index from database and display
